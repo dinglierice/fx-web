81e06d903226ce02c4745c545f637db82abb3b40
dinglie <dinglie.dl@fliggy.com>
2024-09-15 22:41:17 +0800

    feat(cmd, internal/utils): 添加提示命令和加密功能实现

---
+++ internal/utils/encrypt.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+)
+
+func Encrypt(key []byte, text string) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	b := base64.StdEncoding.EncodeToString([]byte(text))
+	ciphertext := make([]byte, aes.BlockSize+len(b))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(b))
+
+	return base64.URLEncoding.EncodeToString(ciphertext), nil
+}
+
+// Decrypt decrypts cipher text string into plain text string using AES algorithm.
+func Decrypt(key []byte, cryptoText string) (string, error) {
+	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return "", errors.New("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	data, err := base64.StdEncoding.DecodeString(string(ciphertext))
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
